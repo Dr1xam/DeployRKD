4e1e99ce1c88f2c1aca90f7756e496e6e8925cdd
Dr1xam <maksimdracenko280@gmail.com>
2026-08-29 09:16:40 +0300

    chore: initial commit

---
+++ backend/src/main/java/com/example/deeployrkd/DeeployRkdApplication.java
@@ -0,0 +1,13 @@
+package com.example.deeployrkd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeeployRkdApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DeeployRkdApplication.class, args);
+    }
+
+}
---
+++ backend/src/test/java/com/example/deeployrkd/DeeployRkdApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.deeployrkd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DeeployRkdApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
